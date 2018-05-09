ad5c4666dd6e55d876e1fe3cd41142a9cc3cba93
Zoraida Alonso <z.alonso.m@gmail.com>
2018-05-09 23:01:55 +0200

    versión inicial

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("La calificación real de " + args[0] + " es " + args[2]);
+        System.out.println("Tu calificación esperada era " + args[1]);
+        System.out.println(veredicto(args[1],args[2]));
+    }
+
+    public static String veredicto(String valor1, String valor2){
+        if (valor1.equals(valor2)) {
+            return "Perfecto. Has tenido buen criterio.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Has mejorado, o te ha llegado el éxito inesperado.";
+        }
+        return "Te has confiado. Falta realismo.";
+    }
+
+}
+
